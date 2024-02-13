134b620401c8c2ee93a6fbb83d1ae8318975b1b7
Sibusiso Mhlanga <Sibusiso.Mhlanga@multichoice.co.za>
2024-02-13 16:37:29 +0200

    Updates on Javascript files paths

--- JavaScripts/gsJQuery.js
+++ JavaScripts/gsJQuery.js
@@ -1,10 +1,15 @@
 
 
 // Animation onload
-$(".portfolio_circle").slideDown(2000);
-$(".portfolio_circle").css("display", "inline-block");
-$("#home_page_jambotron").slideDown(2000);
-$("#home_page_jambotron").css("display", "inline-block");
+
+$(document).ready(function () {
+   $("#home_page_jambotron").slideDown(2000);
+   $("#home_page_jambotron").css("display", "inline-block");
+   $(".portfolio_circle").slideDown(2000);
+   $(".portfolio_circle").css("display", "inline-block");
+
+});
+
 
 // view button for portfolio information window - Click to view the portfolion information
 $("#view1").click(function () {
@@ -18,7 +23,7 @@ $("#view2").click(function () {
    $("#personal_data").css("display", "none");
    $("#work_experience_data").css("display", "none");
    $(".portfolio_circle").css("display", "none");
-   
+
 });
 
 $("#view3").click(function () {
@@ -90,26 +95,26 @@ $(".close_button").mouseout(function () {
 $(document).ready(function () {
    // Hiding/showing drop-down menu
    $('.navbar-toggler').click(function () {
-     $('#navbarTogglerDemo03').slideToggle();
+      $('#navbarTogglerDemo03').slideToggle();
    });
 
    // Animation effect on the jumbotron
    $('#home_page_jambotron').hover(
-     function () {
-       $(this).animate({ fontSize: '2em' }, 'fast');
-     },
-     function () {
-       $(this).animate({ fontSize: '1.5em' }, 'fast');
-     }
+      function () {
+         $(this).animate({ fontSize: '2em' }, 'fast');
+      },
+      function () {
+         $(this).animate({ fontSize: '1.5em' }, 'fast');
+      }
    );
 
    // Chained effects for Save for Later buttons
    $('.save-for-later').click(function () {
-     // Hide the card
-     $(this).closest('.card').slideUp('slow', function () {
-       // Show a message after hiding
-       alert('Item saved for later!');
-     });
+      // Hide the card
+      $(this).closest('.card').slideUp('slow', function () {
+         // Show a message after hiding
+         alert('Item saved for later!');
+      });
    });
- });
+});
 
